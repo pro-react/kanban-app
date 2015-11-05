211be3f4762364641ebf3711e647b47a0faf3b0f
Cássio Zen <cassiozen@gmail.com>
2015-11-05 12:41:30 -0200

    First commit on chapter 1

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+
+/*
+ * Default webpack configuration for development
+ */
+var config = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/app/App.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015','react']
+      }
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true
+  },
+}
+
+/*
+ * If bundling for production, optimize output
+ */
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.plugins = config.plugins.concat([
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ]);
+};
+
+module.exports = config;
