117d1e99c4d5905afd8333e4572b5f2b89724247
Cássio Souza <cassiozen@gmail.com>
2016-05-26 13:02:33 -0300

    Update KanbanApi.js

--- app/api/KanbanApi.js
+++ app/api/KanbanApi.js
@@ -4,6 +4,11 @@ import 'babel-polyfill';
 const API_URL = 'http://kanbanapi.pro-react.com';
 const API_HEADERS = {
   'Content-Type': 'application/json',
+  /*
+   * Change the Authorization to any string you like. It can be your pet's name,
+   * your middle name, your favorite animal, your superpower of choice...
+   * An unique authorization will allow you to have your own environment for cards and tasks
+   */
   Authorization: 'any-string-you-like'
 }
 
